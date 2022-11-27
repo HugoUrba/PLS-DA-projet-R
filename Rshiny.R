a6ac93c853491069d751561c6f9c3377a46d0a54
nawresdh91 <115081929+nawresdh91@users.noreply.github.com>
2022-11-27 13:06:23 +0100

    Add files via upload

---
+++ Rshiny.R
@@ -0,0 +1,64 @@
+breast <- read.xlsx("C:/Users/HP/Downloads/breast-cancer-pls-da.xlsx",sheetIndex=1,header=T)
+print(breast)
+
+
+
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Breast Cancer"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Inputs: Select variables to plot ----
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(
+        inputId = "y",
+        label = "Y-axis:",
+        choices = c("clump", "ucellsize", "ucellshape", "mgadhesion", "sepics","bnuclei", "bchromatin", "normnucl","mitoses","classe"),
+        selected = "classe"
+      ),
+      # Select variable for x-axis
+      selectInput(
+        inputId = "x",
+        label = "X-axis:",
+        choices = c("clump", "ucellsize", "ucellshape", "mgadhesion", "sepics","bnuclei", "bchromatin", "normnucl","mitoses","classe"),
+        selected = "clump"
+      )
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Scree plot ----
+      plotOutput(outputId = "Screeplot")
+      
+    )
+  )
+)
+
+# Define server logic required to draw a scree plot ----
+server <- function(input, output) {
+  output$Screeplot <- renderPlot({
+    plsda.scree(fit)
+  })
+}  
+
+#x    <- faithful$waiting
+#bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+#hist(x, breaks = bins, col = "#75AADB", border = "white",
+#xlab = "Waiting time to next eruption (in mins)",
+#main = "Histogram of waiting times")
+
+#})
+
+#}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
