ee8471ef8b96060bf28d522c8938d414759957b3
nawresdh91 <115081929+nawresdh91@users.noreply.github.com>
2022-11-27 15:18:16 +0100

    Update Rshiny.R

--- Rshiny.R
+++ Rshiny.R
@@ -55,16 +55,6 @@ server <- function(input, output) {
   })
 }  
 
-#x    <- faithful$waiting
-#bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-#hist(x, breaks = bins, col = "#75AADB", border = "white",
-#xlab = "Waiting time to next eruption (in mins)",
-#main = "Histogram of waiting times")
-
-#})
-
-#}
 
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
