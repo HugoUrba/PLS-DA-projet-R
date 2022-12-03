04ae82f3eedca1c419989d26ecc9fb8bcf967ebb
Chrys_Grosso <114347666+Xhrys69@users.noreply.github.com>
2022-12-03 16:29:55 +0100

    Update and rename plsda-cv.R to plsda.cv.R
    small corrections

--- R/plsda.cv.R
+++ R/plsda.cv.R
@@ -1,6 +1,6 @@
 #' Cross Validation for Partial Least Squares Discriminant Analysis
 #'
-#' This function performs a k-cross-validation in order to determine the number of components \code{ncomp}
+#' This function performs a k-cross-validation in order to determine the right number of components \code{ncomp}
 #' to use in \code{plsda.fit} function.
 #' @param
 #' formula an object of class "formula" (or one that can be coerced to that class):
@@ -8,41 +8,41 @@
 #' @param
 #' nfold the number of folds used for cross-validation (k=5 by default).
 #' @return
-#' \code{ncomp} the number of components that must be used in plsda.fit.
+#' \code{ncomp} the number of components that should be used in plsda.fit.
 #' \cr
-#' \code{RESS} a vector containing the calculated PRESS for each components.
+#' \code{RESS} a vector containing the calculated RESS for each components.
 #' \cr
 #' \code{min.RESS}the minimum value of the vector PRESS that has been calculated.
 #' @examples
 #' plsda.cv(Species~., data = iris)
 #' plsda.cv(Species~.,data=iris, nfold = 5)
 
-#création de la fct S3 cross validation
+#design of the S3 cross-validation function
 plsda.cv <- function (formula,data,nfold=5){
-  #controle de la classe formula
+  #check of the formula class
   if(plyr::is.formula(formula)== FALSE ){
     print("formula must be R formula")
   }
-  # controle du data: dataframe or matrix
+  #check of the data: dataframe or matrix
   ok <- (is.data.frame(data) | is.matrix(data))
   if (!ok){
     stop("data should be a dataframe or a matrix")
   }
-  #Diviser le jeu de données en un ensemble de données (X) et une variable cible (y)
+  #split of the dataset into a set of data X and a target-variable (y)
   X <- model.matrix(formula, data = data)
-  X <- X[,-1] #suppression de l'intercept
+  X <- X[,-1] ##removal of the intercept
   Y <- model.response(model.frame(formula, data = data))
-  #nombre d'observations dans chaque Fold
+  #number of observations in each fold 
   n <- trunc(nrow(X)/nfold)
-  #reproduire le meme resultat
+  #reproducing the same output
   set.seed(1)
   s<-sample(1:nrow(X),nrow(X))
 
   newX <- X[s,]
   newY <- Y[s]
-  #nombre de composantes par défaut est le rang de la matrice X
+  #number of components : per default = matrix X rank
   ncomp <- min(nrow(X), ncol(X))
-  #creation de l'instance
+  #design of the instance
   instance <- list()
   for(j in 1:ncomp){
     for (i in (1:nfold)){
@@ -64,12 +64,12 @@ plsda.cv <- function (formula,data,nfold=5){
       #calcul du residual sum of Squares pour le i ème échantillon
       instance$RESS[i] <- mean(instance$y.val != instance$predict)
     }
-    #calcul du RESS pour chaque composante
+    #calculation of the RESS for each component
     instance$RESS[j] <-as.numeric(sum(instance$RESS[i]))
   }
-  #choisir le ncomp qui correspond au min de RESS
+  #choice of the ncomp with the lowest RESS
   instance$ncomp <- which.min(instance$RESS)
-  #retourner le RESS correspondant
+  #return the corresponding RESS
   instance$RESS <- instance$RESS[ instance$ncomp]
   class(instance) <- "cross_validation"
   return(instance)
